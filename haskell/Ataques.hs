c9fd9f6901da757fb2007673999335a5144f3105
Dante de Araújo Costa <66438227+danteacosta@users.noreply.github.com>
2021-08-23 16:01:21 -0300

    func relacionando tipos e arrays com chances nos efeitos

--- haskell/Ataques.hs
+++ haskell/Ataques.hs
@@ -5,7 +5,7 @@ import System.IO.Unsafe
 import MenusGraficos
 
 {-___________________________________________________ metodo para designar ataques ___________________________________________________-}
-designaAtaque :: Int -> Int 
+designaAtaque :: Int -> Int
 designaAtaque 1 = curar
 designaAtaque 2 = atacar
 designaAtaque 3 = ataqueCritico
@@ -36,6 +36,15 @@ opcoesDeCritico = [0,0,0,0,40]
 ataqueCritico :: Int
 ataqueCritico = opcoesDeCritico !!unsafePerformIO (getStdRandom (randomR (0, 4)))
 
-
+opcoesDeStatus=[0,0,0,10,10]
 alteraStatus :: Int
-alteraStatus = error "not implemented"
+alteraStatus = opcoesDeCritico !!unsafePerformIO (getStdRandom (randomR (0, 4)))
+--Usar para rodar nos turnos e ver se o efeito sai depois nos rounds
+tiraStatus=[false,true,true,true,false]
+--Pensar como pegar os tipos do pokemon da batalha no txt
+danoPorTipo :: String -> String -> Int
+danoPorTipo a b
+  | a=="Fogo" && b=="Gelo" || (a=="Terrestre" && b=="Fogo") || ((a=="Terrestre" && b=="Elétrico") || (a=="Terrestre" && b=="Poison")) = 10
+  | a=="Terrestre" && b=="Gelo" || a=="Poison" && b=="Terrestre" = -5
+  | a=="Elétrico" && b=="Terrestre" || a=="Fogo" && b=="Terrestre" = -5
+  | otherwise =0
