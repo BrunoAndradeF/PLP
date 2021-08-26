5fe8a2db293a20b05fb000c9e459289d5b4ad3f6
Dante de Araújo Costa <66438227+danteacosta@users.noreply.github.com>
2021-08-26 20:40:28 -0300

    mudei o tipo para nome do pokemon

--- haskell/Ataques.hs
+++ haskell/Ataques.hs
@@ -25,7 +25,7 @@ atacar = unsafePerformIO (getStdRandom (randomR (5, 25)))
 ataqueCritico :: Int
 ataqueCritico = unsafePerformIO (getStdRandom (randomR (10, 40)))
 
-{-opcoesDeStatus=[0,0,0,10,10]
+opcoesDeStatus=[0,0,0,10,10]
 alteraStatus :: Int
 alteraStatus = opcoesDeCritico !!unsafePerformIO (getStdRandom (randomR (0, 4)))
 --Usar para rodar nos turnos e ver se o efeito sai depois nos rounds
@@ -33,7 +33,7 @@ tiraStatus=[false,true,true,true,false]
 --Pensar como pegar os tipos do pokemon da batalha no txt
 danoPorTipo :: String -> String -> Int
 danoPorTipo a b
-  | (a=="Fogo" && b=="Gelo") || (a=="Terrestre" && b=="Fogo") || ((a=="Terrestre" && b=="Elétrico") || (a=="Terrestre" && b=="Poison")) = 10
-  | (a=="Terrestre" && b=="Gelo") || (a=="Poison" && b=="Terrestre") = -5
-  | (a=="Elétrico" && b=="Terrestre") || (a=="Fogo" && b=="Terrestre") = -5
-  | otherwise =0-}
+  | (a=="Zeca Skull" && b=="SeaHourse") || (a=="Digglet" && b=="Zeca Skull") || ((a=="Digglet" && b=="Pikachu") || (a=="Digglet" && b=="Kakuna")) = 10
+  | (a=="Digglet" && b=="SeaHourse") || (a=="Kakuna" && b=="Digglet") = -5
+  | (a=="Pikachu" && b=="Digglet") || (a=="Zeca Skull" && b=="Digglet") = -5
+  | otherwise =0
