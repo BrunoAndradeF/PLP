9ca476461f3ef6933dbeb24400bba2831655307b
Dante de Araújo Costa <66438227+danteacosta@users.noreply.github.com>
2021-09-20 22:44:29 -0300

    adicionei os times para o modo historia e arq p/ a batalha em si

---
+++ prolog/pokemon.pl
@@ -0,0 +1,1 @@
+:-include('pokemon_data.pl').
--- prolog/pokemon_data.pl
+++ prolog/pokemon_data.pl
@@ -1,3 +1,4 @@
+/*Pokemons do jogo*/
 
 pokemon_stats(Zeca Skull, [fogo],100).
 pokemon_stats(Eevee, [normal],100).
@@ -6,6 +7,7 @@ pokemon_stats(Pikachu, [eletrico],100).
 pokemon_stats(Kakuna, [poison],100).
 pokemon_stats(Digglet,[ground],100)
 
+/*Lista de tipos de pokemon*/
 pokemon_types([normal, fogo, gelo, eletrico, poison, ground]).
 
 type_chart_attack(normal, [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]).
@@ -14,3 +16,8 @@ type_chart_attack(gelo, [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]).
 type_chart_attack(eletrico, [1.0, 1.0, 1.0, 1.0, 1.0, 0.5]).
 type_chart_attack(poison, [1.0, 1.0, 1.0, 1.0, 1.0, 0.5]).
 type_chart_attack(ground, [1.0, 2.0, 0.5, 2.0, 2.0, 1.0]).
+
+/* Fazendo os times do modo história*/
+pokemon_trainer(ash, [Pikachu, Zeca Skull, Digglet]).
+pokemon_trainer(misty, [Kakuna,Seahourse]).
+pokemon_trainer(brock, [Eevee]).
